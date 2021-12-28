697010f68667b0d1ff909f6b1bc9eae0cc34f246
quyen <hoangquyen99@gmail.com>
2021-12-28 11:53:17 +0700

    updated 28/12

--- components/landingpage/info-product/InfoProduct.js
+++ components/landingpage/info-product/InfoProduct.js
@@ -4,14 +4,11 @@ import { useEffect } from "react";
 
 // image
 import product1 from "../../../public/image/product1.png";
-import product2 from "../../../public/image/product2.png";
-import product3 from "../../../public/image/product3.png";
-import product4 from "../../../public/image/product4.png";
-import iconLogo from "../../../public/image/icon_logo.png";
 import Head from "next/head";
 
 //css class
 import styles from "./InfoProduct.module.css";
+import Link from "next/link";
 
 // wow
 const isServer = typeof window === "undefined";
@@ -30,71 +27,58 @@ export default function InfoProduct() {
         />
       </Head>
       <div className="row align-items-center">
-        <div className="col-lg-5">
+        <div className="col-lg-6">
           <h1 className={`${styles.title} wow fadeInDown`}>
             Sarmentosa immune support
           </h1>
-          <p className="wow fadeInUp" data-wow-delay="500ms">
+          <p
+            className={`${styles.infoText} wow fadeInUp`}
+            data-wow-delay="500ms"
+          >
             Là đề tài nghiên cứu khoa học, với hàm lượng Saponin, Polyphenol,
             Muối vô Cơ, Vitamin rất cao được chiếc xuất từ loài Sa Sâm Việt quý
             giá, được trồng từ một quy trình đặc biệt duy nhất tại Việt Nam và
             trên Thế giới.
           </p>
           <br />
-          <p className="wow fadeInUp" data-wow-delay="800ms">
+          <p
+            className={`${styles.infoText} wow fadeInUp`}
+            data-wow-delay="800ms"
+          >
             <i>Công Ty Cổ Phần Tập Đoàn Dược Mỹ Phẩm Sa Sâm Việt </i> phối hợp
             cùng các nhà khoa học các nhà bào chế phối cùng các dược thảo thiên
             nhiên quý hiếm của Việt Nam bào chế tạo nên một sản phẩm đẳng cấp,
             tuyệt vời giúp hỗ trợ bổ phế, hóa đờm; giúp giảm ho gió, họ khan, ho
             có đờm, ho lâu ngày.
           </p>
-          <div className="text-center">
-            <button type="button" className={`btn ${styles.btn}`}>
-              Xem thêm
-            </button>
+          <div
+            className={` ${styles.saleProduct} wow fadeInUp `}
+            data-wow-delay="1s"
+          >
+            <div className="d-flex justify-content-center align-items-center ">
+              <p className={styles.listedPrice}>
+                200.000<span>đ</span>
+              </p>
+              <p className={styles.promotionalPrice}>
+                180.000<span>đ</span>
+              </p>
+            </div>
+            <p className={styles.saleInfo}>
+              Giảm giá lên đến <span className={styles.discount}>20%</span>
+            </p>
+          </div>
+          <div className="text-center wow fadeInUp" data-wow-delay="1.4s">
+            <Link href="/my-cart">
+              <a className={`btn ${styles.btn}`}>Mua ngay</a>
+            </Link>
           </div>
         </div>
-        <div className="col-lg-7 position-relative mt-5 mt-lg-0">
-          <div className="row">
-            <div className={` col-6 `}>
-              <Image
-                src={product1}
-                alt="product1"
-                className={`${styles.imageProduct1} wow fadeInLeft`}
-                data-wow-duration="1.2s"
-                data-wow-delay="200ms"
-              />
-            </div>
-            <div className="col-6 pt-4">
-              <Image
-                src={product2}
-                alt="product2"
-                className={`${styles.imageProduct2} wow fadeInRight`}
-                data-wow-duration="1.2s"
-                data-wow-delay="400ms"
-              />
-            </div>
-            <div className="col-6 pt-3">
-              <Image
-                src={product3}
-                alt="product3"
-                className={`${styles.imageProduct3} wow fadeInLeft`}
-                data-wow-duration="1.2s"
-                data-wow-delay="600ms"
-              />
-            </div>
-            <div className="col-6 ">
-              <Image
-                src={product4}
-                alt="product4"
-                className={`${styles.imageProduct4} wow fadeInRight`}
-                data-wow-duration="1.2s"
-                data-wow-delay="800ms"
-              />
-            </div>
-          </div>
-          <div className={`${styles.iconLogo} `}>
-            <Image src={iconLogo} alt="iconLogo" width={40} height={40} />
+        <div className="col-lg-6  mt-5 mt-lg-0">
+          <div
+            className={`${styles.imageProduct1} wow fadeInRight`}
+            data-wow-duration="1.4s"
+          >
+            <Image src={product1} alt="product1" className={`  img-fluid`} />
           </div>
         </div>
       </div>
--- components/landingpage/ingredient/Ingredient.js
+++ components/landingpage/ingredient/Ingredient.js
@@ -1,24 +1,42 @@
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Lightbox from "react-image-lightbox";
 
 //css
 import styles from "./Ingredient.module.css";
+import "react-image-lightbox/style.css";
 
 //images
 import ingredientImage from "../../../public/image/ingredient.png";
 import ingredientLine from "../../../public/image/ingredient_line_top.png";
 import leaf from "../../../public/image/leaf.png";
 import leaf1 from "../../../public/image/leaf1.png";
-import ingreSmall from "../../../public/image/ingre_small.png";
+import ingreLeft1 from "../../../public/image/ingreLeft1.png";
+import ingreLeft2 from "../../../public/image/ingreRight1.png";
+import ingreLeft3 from "../../../public/image/ingreLeft3.png";
+import ingreRight1 from "../../../public/image/ingreLeft2.png";
+import ingreRight2 from "../../../public/image/ingreRight2.png";
+import ingreRight3 from "../../../public/image/ingreRight3.png";
+import product from "../../../public/image/product.png";
 
 // wow
 const isServer = typeof window === "undefined";
 const WOW = !isServer ? require("wowjs") : null;
+const images = [
+  "https://res.cloudinary.com/du7hfrsyf/image/upload/v1640666257/My%20Images/4_ug67mz.png",
+  "https://res.cloudinary.com/du7hfrsyf/image/upload/v1640666257/My%20Images/1_rgdbrb.png",
+  "https://res.cloudinary.com/du7hfrsyf/image/upload/v1640666505/My%20Images/5_kmh6mu.png",
+  "https://res.cloudinary.com/du7hfrsyf/image/upload/v1640666257/My%20Images/3_rn4z4e.png",
+  "https://res.cloudinary.com/du7hfrsyf/image/upload/v1640666504/My%20Images/6_ajzlp1.png",
+  "https://res.cloudinary.com/du7hfrsyf/image/upload/v1640666257/My%20Images/2_zexvjh.png",
+];
 
 export default function Ingredient() {
   useEffect(() => {
     new WOW.WOW().init();
   }, []);
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={`${styles.ingredient}`}>
       <Image src={ingredientLine} alt="ingredientLine" />
@@ -31,24 +49,17 @@ export default function Ingredient() {
         />
       </div>
 
-      <div className="container pb-5">
+      <div className=" pb-5">
         <h1 className={`text-center ${styles.title} mt-5`}>Thành phần</h1>
-        <div className={`row  ${styles.content}`}>
-          <div className="col-md-6 col-lg-4">
+        <div className={`row  ${styles.content} justify-content-center`}>
+          <div className="col-md-6 col-lg-3">
             <div
               className={` ${styles.ingredientItem} d-flex justify-content-end align-items-center`}
             >
-              <div className="d-block d-lg-none">
-                <Image
-                  src={ingreSmall}
-                  alt="ingreSmall"
-                  width="70"
-                  height="70"
-                />
-              </div>
+              <div className="d-block d-lg-none"></div>
 
-              <h4>
-                Cát cánh <span>( Platycodon grandiflorum )</span>
+              <h4 className="text-right">
+                Cát cánh <p>( Platycodon grandiflorum )</p>
               </h4>
             </div>
             <div
@@ -56,14 +67,14 @@ export default function Ingredient() {
             >
               <div className="d-block d-lg-none">
                 <Image
-                  src={ingreSmall}
-                  alt="ingreSmall"
+                  src={ingreLeft1}
+                  alt="ingreLeft1"
                   width="70"
                   height="70"
                 />
               </div>
-              <h4>
-                Bán hạ <span>( Typhonium divaricatum Decne )</span>
+              <h4 className="text-right">
+                Bán hạ <p>( Typhonium divaricatum Decne )</p>
               </h4>
             </div>
             <div
@@ -71,34 +82,148 @@ export default function Ingredient() {
             >
               <div className="d-block d-lg-none">
                 <Image
-                  src={ingreSmall}
-                  alt="ingreSmall"
+                  src={ingreLeft1}
+                  alt="ingreLeft1"
                   width="70"
                   height="70"
                 />
               </div>
-              <h4>
-                Tỳ bà diệp <span>( Eriobotrys japonica )</span>
+              <h4 className="text-right">
+                Tỳ bà diệp <p>( Eriobotrys japonica )</p>
               </h4>
             </div>
           </div>
-          <div className="col-md-6 col-lg-4 d-none d-lg-block ">
-            <Image src={ingredientImage} alt="ingredientImage" />
+          <div className="col-md-5 col-lg-5 d-none d-lg-block ">
+            <div className={styles.ingredientImage}>
+              <Image
+                src={ingredientImage}
+                alt="ingredientImage"
+                className="img-fluid"
+              />
+              <div
+                className={styles.ingreLeft1}
+                onClick={() => {
+                  setIsOpen(true);
+                  setPhotoIndex(0);
+                }}
+              >
+                <Image
+                  src={ingreLeft1}
+                  alt="ingreLeft1"
+                  width="150"
+                  height="150"
+                />
+              </div>
+              <div
+                className={styles.ingreLeft2}
+                onClick={() => {
+                  setIsOpen(true);
+                  setPhotoIndex(1);
+                }}
+              >
+                <Image
+                  src={ingreLeft2}
+                  alt="ingreLeft2"
+                  width="150"
+                  height="150"
+                />
+              </div>
+              <div
+                className={styles.ingreLeft3}
+                onClick={() => {
+                  setIsOpen(true);
+                  setPhotoIndex(2);
+                }}
+              >
+                <Image
+                  src={ingreLeft3}
+                  alt="ingreLeft3"
+                  width="150"
+                  height="150"
+                />
+              </div>
+              <div
+                className={styles.ingreRight1}
+                onClick={() => {
+                  setIsOpen(true);
+                  setPhotoIndex(3);
+                }}
+              >
+                <Image
+                  src={ingreRight1}
+                  alt="ingreRight1"
+                  width="150"
+                  height="150"
+                />
+              </div>
+              <div
+                className={styles.ingreRight2}
+                onClick={() => {
+                  setIsOpen(true);
+                  setPhotoIndex(4);
+                }}
+              >
+                <Image
+                  src={ingreRight2}
+                  alt="ingreRight2"
+                  width="150"
+                  height="150"
+                />
+              </div>
+              <div
+                className={styles.ingreRight3}
+                onClick={() => {
+                  setIsOpen(true);
+                  setPhotoIndex(5);
+                }}
+              >
+                <Image
+                  src={ingreRight3}
+                  alt="ingreRight3"
+                  width="150"
+                  height="150"
+                />
+              </div>
+              <div className={styles.product}>
+                <Image src={product} alt="product" width="100" height="200" />
+              </div>
+            </div>
+
+            {/* lightbox */}
+            {isOpen && (
+              <Lightbox
+                mainSrc={images[photoIndex]}
+                nextSrc={images[(photoIndex + 1) % images.length]}
+                prevSrc={
+                  images[(photoIndex + images.length - 1) % images.length]
+                }
+                onCloseRequest={() => setIsOpen(false)}
+                onMovePrevRequest={() =>
+                  setPhotoIndex(
+                    (photoIndex + images.length - 1) % images.length
+                  )
+                }
+                onMoveNextRequest={() =>
+                  setPhotoIndex((photoIndex + 1) % images.length)
+                }
+              />
+            )}
+            {/* End Lightbox */}
           </div>
-          <div className="col-md-6 col-lg-4">
+          <div className="col-md-6 col-lg-3">
             <div
               className={` ${styles.ingredientItem} d-flex justify-content-start align-items-center`}
             >
               <div className="d-block d-lg-none">
                 <Image
-                  src={ingreSmall}
-                  alt="ingreSmall"
+                  src={ingreLeft1}
+                  alt="ingreLeft1"
                   width="70"
                   height="70"
                 />
               </div>
               <h4>
-                Sa Sâm Việt <span>( Sarmentosa )</span>
+                Sa Sâm Việt <p>( Sarmentosa )</p>
               </h4>
             </div>
             <div
@@ -106,14 +231,14 @@ export default function Ingredient() {
             >
               <div className="d-block d-lg-none">
                 <Image
-                  src={ingreSmall}
-                  alt="ingreSmall"
+                  src={ingreLeft1}
+                  alt="ingreLeft1"
                   width="70"
                   height="70"
                 />
               </div>
               <h4>
-                Bạc hà diệp <span>( Menthg Arvensis Lin )</span>
+                Bạc hà diệp <p>( Menthg Arvensis Lin )</p>
               </h4>
             </div>
             <div
@@ -121,14 +246,14 @@ export default function Ingredient() {
             >
               <div className="d-block d-lg-none">
                 <Image
-                  src={ingreSmall}
-                  alt="ingreSmall"
+                  src={ingreLeft1}
+                  alt="ingreLeft1"
                   width="70"
                   height="70"
                 />
               </div>
               <h4>
-                Tang bạch bi <span>( Cortex mori Albae Radicis )</span>
+                Tang bạch bi <p>( Cortex mori Albae Radicis )</p>
               </h4>
             </div>
           </div>
