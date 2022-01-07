0aedaafdb006a0bb97520f67374e3a5a0300312e
quyen <hoangquyen99@gmail.com>
2022-01-07 16:23:12 +0700

    updated 7/1

--- components/landingpage/carousel/Carousel.js
+++ components/landingpage/carousel/Carousel.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import ReactPlayer from "react-player";
 
@@ -15,7 +15,7 @@ import iconHand from "../../../public/image/icon_flower.png";
 import styles from "./Carousel.module.css";
 
 export default function Carousel() {
-  const videoRef = useRef();
+  // const videoRef = useRef();
 
   // useEffect
   // useEffect(() => {
@@ -32,17 +32,13 @@ export default function Carousel() {
     new WOW.WOW().init();
   }, []);
 
+  const [played, setPlayed] = useState(false);
+  // console.log("Player", played);
+
   return (
     <div>
       <div>
         <div className={styles.bgvideo}>
-          {/* <video ref={videoRef} autoPlay loop muted className={styles.video}>
-            <source src="polina.webm" type="video/webm" />
-            <source
-              src="https://res.cloudinary.com/dn4nxz7f0/video/upload/v1594348575/Pexels_Videos_1093655_pr26ax.mp4"
-              type="video/mp4"
-            />
-          </video> */}
           <ReactPlayer
             url="https://www.youtube.com/watch?v=eVXvzyET120&ab_channel=SaS%C3%A2mVi%E1%BB%87t"
             muted={false}
@@ -52,6 +48,11 @@ export default function Carousel() {
             volume={0}
             playbackRate={1}
             className={styles.video}
+            onProgress={(progress) => {
+              if (progress.playedSeconds >= 5 && played === false) {
+                setPlayed(true);
+              }
+            }}
           />
           <div className={styles.overlay}></div>
           <div className={styles.bgLine}>
@@ -63,16 +64,19 @@ export default function Carousel() {
           <div className={styles.bgLine2}>
             <Image src={bgLine} alt="Line2 background" />
           </div>
+          {!played && (
+            <div
+              className={`${styles.content} wow bounceOutDown`}
+              data-wow-delay="8s"
+            >
+              <p>Dược liệu vùng cát biển</p>
+            </div>
+          )}
         </div>
         <div className={styles.background}>
           <div className={`${styles.logo} `}>
             <Image src={logo} alt="Logo SasamViet" />
           </div>
-        </div>
-        <div className={styles.content}>
-          <p className="wow fadeInUp" data-wow-duration="3s">
-            Dược liệu vùng cát biển
-          </p>
         </div>
       </div>
 
